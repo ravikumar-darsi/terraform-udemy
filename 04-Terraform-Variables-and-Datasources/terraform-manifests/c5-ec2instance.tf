b75966c946da0f45d958808fec0928caec9ad247
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-07-16 17:08:18 +0530

    small modification in c5 and c6 of 04 folder

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c5-ec2instance.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c5-ec2instance.tf
@@ -1,5 +1,5 @@
 //EC2 Instance
-resource "aws_instance" "myec2VM" {
+resource "aws_instance" "myec2vm" {
   ami                    = data.aws_ami.amzlinux2.id
   instance_type          = var.instance_type
   user_data              = file("${path.module}/app1-install.sh")
