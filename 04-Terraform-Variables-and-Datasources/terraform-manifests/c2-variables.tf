79c833f25da2985f4197a4a6469aaf16f8a58dd3
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-07-16 16:47:23 +0530

    variables and other datasource operations are done

---
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c2-variables.tf
@@ -0,0 +1,20 @@
+#Input Variables
+# AWS Region
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+# AWS EC2 Instance Type
+variable "instance_type" {
+  description = "EC2 Instance type"
+  type        = string
+  default     = "t3.micro"
+}
+# AWS Ec2 Instance Key Pair
+variable "instance_keypair" {
+  description = "AWS EC2 Key pair that need to be associated with Ec2 instance"
+  type        = string
+  default     = " "
+}
